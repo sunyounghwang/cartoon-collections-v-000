d83cb83898af286cdfcefcd63fd9bba019d51a12
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-10 20:49:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(calls)
-  calls.map { |call| call.capitalize << "!" }
+  calls.map { |call| call.capitalize + "!" }
 end
 
 def long_planeteer_calls# code an argument here
