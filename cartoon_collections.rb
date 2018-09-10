f7198ef8f7ad178a697b291ef9b388073b427c6e
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-09-10 20:51:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(calls)
   calls.map { |call| call.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+
 end
 
 def find_the_cheese# code an argument here
